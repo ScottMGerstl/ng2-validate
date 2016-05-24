0b6427a53cda13eb4a9f6c2a6f2e793754620209
Scott M Gerstl <scott.gerstl@outlook.com>
2016-05-23 20:55:23 -0500

    testing out desired api structure

---
+++ core.d.ts
@@ -0,0 +1,7 @@
+export { BaseValidation } from './core/models/base.validation';
+export { IFieldValidation } from './core/models/field-validation.interface';
+export { IFieldValidatorResult } from './core/models/field-validator-result.interface';
+export { ValidationMessage } from './core/models/validation-message.model';
+export { ValidationMessages } from './core/models/validation-messages.model';
+export { ValidationMessageComponent } from './core/components/validation-message/validation-message.component';
+export { ValidationMessagesComponent } from './core/components/validation-messages/validation-messages.component';
---
+++ core.js
@@ -0,0 +1,12 @@
+"use strict";
+var base_validation_1 = require('./core/models/base.validation');
+exports.BaseValidation = base_validation_1.BaseValidation;
+var validation_message_model_1 = require('./core/models/validation-message.model');
+exports.ValidationMessage = validation_message_model_1.ValidationMessage;
+var validation_messages_model_1 = require('./core/models/validation-messages.model');
+exports.ValidationMessages = validation_messages_model_1.ValidationMessages;
+var validation_message_component_1 = require('./core/components/validation-message/validation-message.component');
+exports.ValidationMessageComponent = validation_message_component_1.ValidationMessageComponent;
+var validation_messages_component_1 = require('./core/components/validation-messages/validation-messages.component');
+exports.ValidationMessagesComponent = validation_messages_component_1.ValidationMessagesComponent;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29yZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInNyYy9jb3JlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxnQ0FBK0IsK0JBQStCLENBQUM7QUFBdEQsMERBQXNEO0FBRy9ELHlDQUFrQyx3Q0FBd0MsQ0FBQztBQUFsRSx5RUFBa0U7QUFDM0UsMENBQW1DLHlDQUF5QyxDQUFDO0FBQXBFLDRFQUFvRTtBQUM3RSw2Q0FBMkMsbUVBQW1FLENBQUM7QUFBdEcsK0ZBQXNHO0FBQy9HLDhDQUE0QyxxRUFBcUUsQ0FBQztBQUF6RyxrR0FBeUcifQ==
