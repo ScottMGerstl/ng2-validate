51204fd777e295db0326ffd91760c1dc8abf2c09
Scott M Gerstl <scott.gerstl@outlook.com>
2016-06-02 18:50:45 -0500

    Added a form

---
+++ demo/email.validator.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { AbstractControl } from '@angular/common';
+import { BaseValidation, ValidationMessages, IFieldValidation, IFieldValidatorResult } from '../core';
+import { HasValue, IsEmail, MaxLength } from '../logic';
+
+// define an interface for configuration
+export interface IEmailValidationConfig {
+    required: boolean,
+    maxLength: number
+}
+
+/**
+ * create a class that can be imported into a component. This one:
+ * extends from the built in BaseValidation class which handles configuration defaulting.
+ * implements IFieldValidation which helps standardize validation access and return types
+ */
+export class EmailValidation extends BaseValidation<IEmailValidationConfig> implements IFieldValidation {
+
+    // accept a configuration. any unspecified values will be defaulted based on getDefaultConfig()
+    constructor(config?: IEmailValidationConfig) {
+        super(config);
+    }
+
+    // returns a function that yields a result equivalent to angular2's ValidatorFn (ValidatorFn is not public)
+    public get validator(): (control: AbstractControl) => IFieldValidatorResult {
+
+        return (control: AbstractControl): IFieldValidatorResult => {
+
+            // create a ValidationMessages list
+            let result: ValidationMessages = new ValidationMessages();
+
+            // Determine if a value is present. If not, determine if it is required. If required, add a message
+            if (HasValue.check(control) === false) {
+                if (this.config.required) {
+                    result.addMessage('required', `Email is required`, this.config);
+                }
+            }
+            else {
+
+                // If a value is present to be validated, check the length and pattern
+                if (MaxLength.check(control, this.config.maxLength) === false) {
+                    result.addMessage('maxLength', `Email cannot exceed ${this.config.maxLength} characters`, this.config);
+                }
+
+                if (IsEmail.check(control) === false) {
+                    result.addMessage('isEmail', `Email must be valid`, this.config);
+                }
+            }
+
+            // resolve the list
+            return result.resolve();
+        }
+    }
+
+    // implementation of abstract function in BaseValidation
+    public getDefaultConfig(): IEmailValidationConfig {
+        return EmailValidation.defaultConfig;
+    }
+
+    // static access to a default configuration
+    public static get defaultConfig(): IEmailValidationConfig {
+        return {
+            required: true,
+            maxLength: 256
+        }
+    }
+}
---
+++ demo/form.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Control, ControlGroup } from '@angular/common';
+import { ValidationMessagesComponent } from '../core';
+import { EmailValidation } from './email.validator';
+
+@Component({
+    directives: [ValidationMessagesComponent],
+    selector: 'email-form',
+    template: `
+        <div [ngFormModel]="myForm">
+            <label>Email</label>
+            <input [(ngFormControl)]="emailControl" [(ngModel)]="email"/>
+            <validation-messages [control]="emailControl"></validation-messages>
+        </div>
+    `
+})
+export class FormComponent implements OnInit {
+    private myForm: ControlGroup;
+    private emailControl: Control;
+
+    private email: string;
+
+    public ngOnInit(): void {
+        this.emailControl = new Control('', new EmailValidation().validator);
+
+        this.myForm = new ControlGroup({
+            emailControl: this.emailControl
+        });
+    }
+}
