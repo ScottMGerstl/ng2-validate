80827d864d198a8c3470e6b6b0407bff61744263
Scott M Gerstl <scott.gerstl@outlook.com>
2016-06-02 17:55:29 -0500

    Started setting up demo. Checking progree

---
+++ demo/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+// import { ROUTER_DIRECTIVES, Routes, Router } from '@angular/router';
+// import { ModalDemoComponent } from './modal-demo.component';
+// import { HelloComponent } from './hello.component';
+
+@Component({
+    selector: 'modal-demo',
+    template: 'Hi demo'//,
+    // directives: [ROUTER_DIRECTIVES]
+})
+// @Routes([
+//     { path: '/', component: ModalDemoComponent },
+//     { path: '/hello', component: HelloComponent }
+// ])
+export class AppComponent implements OnInit {
+
+    // constructor(private router: Router) { }
+
+    ngOnInit() {
+        // this.router.navigate(['/']);
+    }
+}
