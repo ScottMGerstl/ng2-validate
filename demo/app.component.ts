6d2f5820f0ff18792d5b6f17f706e142c7c78df6
Scott M Gerstl <scott.gerstl@outlook.com>
2016-06-02 18:52:57 -0500

    added form to directive in app

--- demo/app.component.ts
+++ demo/app.component.ts
@@ -3,7 +3,8 @@ import { FormComponent } from './form.component';
 
 @Component({
     selector: 'demo-app',
-    template: '<email-form></email-form>'
+    template: '<email-form></email-form>',
+    directives: [FormComponent]
 })
 export class AppComponent {
 }
